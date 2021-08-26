89fc616f6a9db75988813ae70515518527b178b9
TTFTCUTS <emperorlexx@hotmail.com>
2021-08-26 06:05:03 +0100

    Connections and stuff

--- web/endlessdungeon/world/biome.dart
+++ web/endlessdungeon/world/biome.dart
@@ -1,5 +1,7 @@
 import "package:CommonLib/Collection.dart";
+import "package:CommonLib/Random.dart";
 
+import '../cache.dart';
 import 'tile.dart';
 
 typedef BiomeConnectionGenerator = Iterable<BiomeConnection> Function(int x, int y, bool rightEdge);
@@ -33,6 +35,34 @@ class BiomeConnection {
     BiomeConnection(num position, num size): this.position = position.toDouble(), this.size = size.toDouble();
 
     static Iterable<BiomeConnection> defaultConnection(int x, int y, bool rightEdge) sync* {
-        yield new BiomeConnection(DungeonTile.halfSize, 10);
+        //yield new BiomeConnection(DungeonTile.halfSize, 10);
+
+        final Random rand = new Random(hashPair(x, y) + (rightEdge ? 1 : 0));
+
+        final double countRoll = rand.nextDouble();
+
+        int count;
+
+        if (countRoll < 0.35) {
+            count = 0;
+        } else if (countRoll < 0.7) {
+            count = 1;
+        } else if (countRoll < 0.90) {
+            count = 2;
+        } else if (countRoll < 0.99){
+            count = 3;
+        } else {
+            count = 3 + rand.nextInt(5);
+        }
+
+        if (count == 0) { return; }
+
+        for (int i=0; i<count; i++) {
+            final double size = rand.nextDouble() * rand.nextDouble() * 20 + 8;
+            final double limit = size * 0.5 + 5;
+            final double segmentSize = DungeonTile.tileSize / count;
+
+            yield new BiomeConnection( segmentSize * i + limit + rand.nextDouble(segmentSize - limit*2), size);
+        }
     }
 }
--- web/endlessdungeon/world/tile.dart
+++ web/endlessdungeon/world/tile.dart
@@ -10,7 +10,12 @@ class DungeonTile {
     final Dungeon dungeon;
     late final Biome biome;
 
-
+    final Map<TileSide,Set<BiomeConnection>> connections = <TileSide,Set<BiomeConnection>> {
+        TileSide.top: <BiomeConnection>{},
+        TileSide.bottom: <BiomeConnection>{},
+        TileSide.left: <BiomeConnection>{},
+        TileSide.right: <BiomeConnection>{}
+    };
 
     final int x;
     final int y;
@@ -36,15 +41,44 @@ class DungeonTile {
             ctx.fillStyle = biome.colour;
         }
 
-        ctx.fillRect(worldX - halfSize + 0.5, worldY - halfSize + 0.5, tileSize, tileSize);
+        ctx.fillRect(worldX - halfSize, worldY - halfSize, tileSize, tileSize);
 
         ctx
             ..strokeStyle = "black"
-            ..strokeRect(worldX - halfSize + 0.5, worldY - halfSize + 0.5, tileSize, tileSize)
+            //..strokeRect(worldX - halfSize + 0.5, worldY - halfSize + 0.5, tileSize, tileSize)
             ..fillStyle = "black"
             ..textAlign = "center"
             ..fillText("Tile $x,$y: ${generated ? biome.name : "Generating"}", worldX, worldY)
         ;
+
+        ctx
+            ..fillStyle = "black"
+            ..strokeStyle = "rgba(0,0,0,0.3)"
+        ;
+        double conX, conY, conHW, conHH;
+        for (final TileSide side in TileSide.all()) {
+            for (final BiomeConnection connection in connections[side]!) {
+                conHW = connection.size * 0.5 * side.xDir;
+                conHH = connection.size * 0.5 * side.yDir;
+
+                conX = worldX - halfSize + (tileSize * side.xPos) + (connection.position * side.xDir);
+                conY = worldY - halfSize + (tileSize * side.yPos) + (connection.position * side.yDir);
+
+                ctx.fillRect(
+                    conX - conHW - 1,
+                    conY - conHH - 1,
+                    conHW * 2 + 3,
+                    conHH * 2 + 3
+                );
+
+                ctx
+                    ..beginPath()
+                    ..moveTo(conX, conY)
+                    ..lineTo(worldX, worldY)
+                    ..stroke()
+                ;
+            }
+        }
     }
 
     Future<void> requestGeneration() async {
@@ -59,6 +93,14 @@ class DungeonTile {
 
     Future<void> generate() async {
         this.biome = Biome.biomeList.get(dungeon.blob(x,y));
+
+        for(final TileSide side in TileSide.all()) {
+            final Biome neighbourBiome = Biome.biomeList.get(dungeon.blob(x + side.xCoord,y + side.yCoord));
+
+            final BiomeConnectionGenerator generator = biome.getConnectionForBiome(neighbourBiome);
+
+            connections[side]!.addAll(generator(side.vertical ? side.offsetGenCoords ? x-1 : x : x, side.vertical ? y : side.offsetGenCoords ? y-1 : y, side.vertical));
+        }
     }
 
     /// Used on the worker side to pack the generated data
@@ -66,6 +108,8 @@ class DungeonTile {
         final Map<String,dynamic> payload = <String,dynamic>{};
 
         payload["biome"] = biome.id;
+
+        payload["sides"] = TileSide.all().map((TileSide side) => connections[side]!.map((BiomeConnection c) => <double>[c.position,c.size]).toList()).toList();
 
         return payload;
     }
@@ -75,5 +119,39 @@ class DungeonTile {
         final Map<dynamic,dynamic> payload = rawPayload;
 
         biome = Biome.biomeList[payload["biome"]];
+
+        int i=0;
+        for(final TileSide side in TileSide.all()) {
+            connections[side]!.addAll(payload["sides"][i++].map<BiomeConnection>((dynamic data) => new BiomeConnection(data[0], data[1])));
+        }
     }
+}
+
+class TileSide {
+    static const TileSide top = TileSide._(0,0,1,0,0,-1,true,false);
+    static const TileSide bottom = TileSide._(0,1,1,0,0,1,false,false);
+    static const TileSide left = TileSide._(0,0,0,1,-1,0,true,true);
+    static const TileSide right = TileSide._(1,0,0,1,1,0,false,true);
+
+    static Iterable<TileSide> all() sync* {
+        yield top;
+        yield bottom;
+        yield left;
+        yield right;
+    }
+
+    /// multiply tile size by xPos and yPos and add to x and y
+    final int xPos;
+    final int yPos;
+    /// multiply offsets for connection by this
+    final int xDir;
+    final int yDir;
+    /// offset for neighbour tile in this direction
+    final int xCoord;
+    final int yCoord;
+
+    final bool offsetGenCoords;
+    final bool vertical;
+
+    const TileSide._(int this.xPos, int this.yPos, int this.xDir, int this.yDir, int this.xCoord, int this.yCoord, bool this.offsetGenCoords, bool this.vertical);
 }
